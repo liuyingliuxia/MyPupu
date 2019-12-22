96d591c9f35af1ad96cc4aa93becb30bfa32c7ff
夏 小 <1652976019@qq.com>
2019-12-22 23:20:33 +0800

    优化了一下下搜索框，没什么大变化

--- app/src/main/java/com/mmm/mypupu/ui/search/SearchActivity.kt
+++ app/src/main/java/com/mmm/mypupu/ui/search/SearchActivity.kt
@@ -3,14 +3,25 @@ package com.mmm.mypupu.ui.search
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import com.githang.statusbar.StatusBarCompat
 import com.mmm.mypupu.R
 import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.item_search_history.*
 import java.nio.file.WatchEvent
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(),TextWatcher {
     //历史记录最大item数 为 6
     val MAX_SEARCH_HISTORY_ITEM = 6
     @RequiresApi(Build.VERSION_CODES.M)
@@ -19,7 +30,53 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
         //设置沉浸式状态栏
         StatusBarCompat.setStatusBarColor(this,getColor(R.color.color1), true)
-        //etSearch.addTextChangedListener()
+
+
         ivBack.setOnClickListener { v -> this.finish()}
+        ivClean.setOnClickListener { v -> run{
+            AlertDialog.Builder(this)
+                .setMessage("确认删除全部搜索历史")
+                .setPositiveButton("确定") {
+                        _,_ -> run{
+                    ivClean.visibility = View.GONE
+                    tvNoHistory.visibility = View.VISIBLE
+                    tvHistoryItem.visibility = View.GONE
+                    }
+                }
+                .setNeutralButton("取消", null)
+                .create()
+                .show()
+            }
+        }
+        etSearch.addTextChangedListener(this)
+        etSearch.setOnEditorActionListener(object :TextView.OnEditorActionListener{
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                    addHistory()
+                }
+                return false
+            }
+        })
+
     }
+
+    override fun afterTextChanged(p0: Editable?) {
+
+
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    fun addHistory () {
+        val mGl :GridLayout = findViewById<GridLayout>(R.id.glContainer)
+        this.layoutInflater.inflate(R.layout.item_search_history, mGl)
+        tvHistoryItem.text = etSearch.text
+        tvHistoryItem.visibility = View.VISIBLE
+    }
+
 }
